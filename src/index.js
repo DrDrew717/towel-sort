9683ef39a0a498da40db3c9292c4c86010982156
DrDrew717 <andyak0717@gmail.com>
2025-08-26 22:44:04 +0600

    feat: add solution to towel-sort algorithmic task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  const result = [];
+  if (matrix === undefined) {
+    return result;
+  }
+  for (let i = 0; i < matrix.length; i += 1) {
+    for (let y = 0; y < matrix[i].length; y += 1) {
+      if (i % 2 === 0) {
+        result.push(matrix[i][y]);
+      } else {
+        result.push(matrix[i][matrix[i].length - 1 - y]);
+      }
+    }
+  }
+  return result;
 };
